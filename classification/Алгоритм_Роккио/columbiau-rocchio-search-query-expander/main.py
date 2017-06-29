2496069a5b07c1bece4f6f9b0f7d4a25f4b11225
Irina Sokolova <isokolova1994@gmail.com>
2017-06-30 01:54:14 +0300

    Добавлен алгоритм Роккио. Выдает ошибку, что не найден urllib2

---
+++ classification/Алгоритм_Роккио/columbiau-rocchio-search-query-expander/main.py
@@ -0,0 +1,161 @@
+'''
+Created on Sep 21, 2012
+
+@author: johnterzis
+
+arguments: <precision> <query>
+
+Contains the main loop of the application
+
+'''
+
+import json
+import sys
+import bingclient
+import constants
+import parser
+import constants
+import logging
+import indexer
+import rocchio
+import common
+import math
+import PorterStemmer
+
+
+#only if run as standalone script (not imported module) does, __name__  attribute defaults to __main__
+#assume first arg is <precision> second is <query>
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.ERROR)
+
+#create all singleton objects
+    arglist = sys.argv
+    if len(arglist) < 3:
+        print ("Usage: <precision> <query>")
+        sys.exit(1) #exit interpreter
+
+    print ('Desired precision@10: {}'.format(arglist[1]))
+
+    precisionTenTarg = float(arglist[1])   #must convert string to float
+    #'eECeOiLBFOie0G3C03YjoHSqb1aMhEfqk8qe7Xi2YMs='
+    #connect to client with key arg[1] and post a query with arg[3], query
+
+    bingClient = bingclient.BingClient(constants.BING_ACCT_KEY)
+    indexer = indexer.Indexer()
+    queryOptimizer = rocchio.RocchioOptimizeQuery(arglist[2])
+
+    firstPass = 1
+    precisionAtK = 0.00
+    expandedQuery = arglist[2]
+    queryWeights = {}
+
+
+    #while precision at 10 is less than desired amt issue a query, obtain new precision metric, expand query, repeat
+    while (precisionAtK < precisionTenTarg):
+        precisionAtK = 0.00 #reset precision each round
+        #PROCESS A QUERY
+
+
+        print ('Parameters')
+        print ('%-20s= %s' % ("Query", expandedQuery))
+        print ('%-20s= %s' % ("Target Precision", precisionTenTarg))
+
+
+
+        indexer.clearIndex()
+
+        if firstPass == 1:
+            result = bingClient.webQuery(arglist[2])
+        else:
+            result = bingClient.webQuery(expandedQuery)
+
+        jsonResult = json.loads(result)  #convert string to json
+        #put result into a list of documents
+        parsedResult = parser.Parser(jsonResult['d']['results'])
+        parsedResult.parser()
+        DocumentList = parsedResult.getDocList()
+
+        print ('Total number of results: %d' % len(DocumentList))
+
+        #to calc precision@10 display documents to user and ask them to categorize as Relevant or Non-Relevant
+        print ('======================')
+
+        # Reset collections for relevant ad nonrelevant documents
+        relevantDocuments = []
+        nonrelevantDocuments = []
+
+        for i in range(len(DocumentList)):
+
+            DocumentList[i]["ID"] = i
+            indexer.indexDocument(DocumentList[i])
+
+
+            print ('Result %d' % (i+1))
+            print ('[')
+            print ('  %-9s: %10s' % ("URL", DocumentList[i]["Url"]))
+            print ('  %-9s: %10s' % ("Title", DocumentList[i]["Title"]))
+            print ('  %-9s: %10s' % ("Summary", DocumentList[i]["Description"]))
+            print (']')
+
+            print ('')
+            sys.stdout.write('Relevant (Y/N)? ')
+            value = raw_input()
+            if value.upper() == 'Y':
+                DocumentList[i]['IsRelevant'] = 1   #1 is true , 0 is false
+                precisionAtK = precisionAtK + 1
+                relevantDocuments.append(i)
+
+            elif value.upper() == 'N':
+                DocumentList[i]['IsRelevant'] = 0   #1 is true , 0 is false
+                nonrelevantDocuments.append(i)
+            else:
+                print ('Invalid value entered!')
+
+
+
+        precisionAtK = float(precisionAtK) / 10  #final precision@10 per round
+
+        print ('')
+        print ('Precision@10 is: {}'.format(float(precisionAtK)))
+        print ('')
+
+        #expand query here by indexing and weighting current document list
+        if (precisionAtK == 0):
+            print ('Below desired precision, but can no longer augment the query')
+            sys.exit()
+
+        print ('Indexing results...')
+        indexer.waitForIndexer() # Will block until indexer is done indexing all documents
+
+
+
+
+        # Print inveretd file
+
+        for term in sorted(indexer.invertedFile, key=lambda posting: len(indexer.invertedFile[posting].keys())):
+            logging.info("%-30s %-2s:%-3d %-2s:%-3d %-3s:%-10f" % (term, "TF", indexer.termsFrequencies[term], "DF", len(indexer.invertedFile[term]), "IDF", math.log(float(len(DocumentList)) / len(indexer.invertedFile[term].keys()),10)))
+
+        print ('======================')
+        print ('FEEDBACK SUMMARY')
+
+
+        if (precisionAtK < precisionTenTarg):
+            print ('')
+            print ('Still below desired precision of %f' % precisionTenTarg)
+            queryWeights = queryOptimizer.Rocchio(indexer.invertedFile, DocumentList, relevantDocuments, nonrelevantDocuments)   #optimize new query here
+
+
+            newTerms = common.getTopTerms(expandedQuery, queryWeights, 2)
+            expandedQuery = expandedQuery + " " + newTerms[0] + " " + newTerms[1]
+            firstPass = 0
+
+            print ('Augmenting by %s %s' % (newTerms[0], newTerms[1]))
+
+
+    #precision@10 is > desired , return query and results to user
+    print ('Desired precision reached, done')
+
+
+
+
---
+++ classification/Алгоритм_Роккио/columbiau-rocchio-search-query-expander/rocchio.py
@@ -0,0 +1,104 @@
+'''
+Implement Rocchio algo on a corpus of relevant documents
+by weighting based on td-idf to iteratively form a new query vector of weightings
+for each unique term across all dictionaries (from invertedFiles) passed into Rocchio
+'''
+import constants
+import math
+import sys
+import PorterStemmer
+
+class RocchioOptimizeQuery:
+
+
+
+    def __init__(self, firstQueryTerm):
+        '''
+        Constructor
+        '''
+        self.query = {}
+        self.query[firstQueryTerm] = 1
+     
+        
+    def Rocchio(self, invertedFile, documentsList, relevantDocs, nonrelevantDocs):
+        '''
+        output new query vector'
+        
+        calculate summation of relevant documents weights 
+        'calculate IDF per inverted file'
+        
+
+        '''
+        p = PorterStemmer.PorterStemmer()
+
+        weights = {}
+        for term in invertedFile.iterkeys():
+            sterm = term
+            if constants.STEM_IN_ROCCHIO:
+                sterm = p.stem(term.lower(), 0,len(term)-1)            
+            weights[sterm] = 0.0    #initialize weight vector for each key in inverted file
+        print ('')
+
+        relevantDocsTFWeights = {}
+        nonrelevantDocsTFWeights = {} 
+
+        # ------------------------------------- #
+        # Compute relevantDocsTFWeights and nonrelevantDocsTFWeights vectors
+        for docId in relevantDocs:
+            doc = documentsList[docId]
+            for term in doc["tfVector"]:
+                sterm = term
+                if constants.STEM_IN_ROCCHIO:
+                    sterm = p.stem(term.lower(), 0,len(term)-1)
+
+                if sterm in relevantDocsTFWeights:
+                    relevantDocsTFWeights[sterm] = relevantDocsTFWeights[sterm] + doc["tfVector"][term]
+                else:
+                    relevantDocsTFWeights[sterm] = doc["tfVector"][term]
+
+        
+        for docId in nonrelevantDocs:
+            doc = documentsList[docId]
+            for term in doc["tfVector"]:
+                sterm = term
+                if constants.STEM_IN_ROCCHIO:
+                    sterm = p.stem(term.lower(), 0,len(term)-1)                
+
+                if sterm in nonrelevantDocsTFWeights:
+                    nonrelevantDocsTFWeights[sterm] = nonrelevantDocsTFWeights[sterm] + doc["tfVector"][term]
+                else:
+                    nonrelevantDocsTFWeights[sterm] = doc["tfVector"][term]
+
+
+        # ------------------------------------- #
+        # Compute Rocchio vector
+        for term in invertedFile.iterkeys():
+            idf = math.log(float(len(documentsList)) / float(len(invertedFile[term].keys())), 10)
+
+
+            sterm = term
+            if constants.STEM_IN_ROCCHIO:
+                sterm = p.stem(term.lower(), 0,len(term)-1)
+
+
+            # Terms 2 and 3 of Rocchio algorithm
+            for docId in invertedFile[term].iterkeys():
+                if documentsList[docId]['IsRelevant'] == 1:
+                    # Term 2: Relevant documents weights normalized and given BETA weight
+                    weights[sterm] = weights[sterm] + constants.BETA * idf * (relevantDocsTFWeights[sterm] / len(relevantDocs))
+                else:
+                    # Term 3: NonRelevant documents weights normalized and given BETA weight
+                    weights[sterm] = weights[sterm] - constants.GAMMA * idf * (nonrelevantDocsTFWeights[sterm]/len(nonrelevantDocs))
+
+            # Term 1 of Rocchio, query terms
+            if term in self.query:
+                self.query[term] = constants.ALPHA * self.query[term] + weights[sterm]   #build new query vector of weights
+            elif weights[sterm] > 0:
+                self.query[term] = weights[sterm]
+
+        return self.query
+    
+            
+        
+        
+    
