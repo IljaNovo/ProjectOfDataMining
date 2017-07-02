bf461240577f871138b58680ebcd46c2d19cc645
Irina Sokolova <isokolova1994@gmail.com>
2017-07-03 00:50:02 +0300

    Добавленно меню

--- main.py
+++ main.py
@@ -82,14 +82,23 @@ child_classif.geometry("400x200")
 child_classif.bind("<Destroy>", exit_prog);
 child_classif.withdraw()
 
-child_classif_btn1 = Button(child_classif, text="Метод классификации 1")
+child_classif_btn1 = Button(child_classif, text="C 4.5")
 child_classif_btn1.pack(fill=X)
 
-child_classif_btn2 = Button(child_classif, text="Метод классификации 2")
+child_classif_btn2 = Button(child_classif, text="k Nearest Neighbors")
 child_classif_btn2.pack(fill=X)
 
-child_classif_btn3 = Button(child_classif, text="Метод классификации 3")
+child_classif_btn3 = Button(child_classif, text="Linear Least Squares Classifier")
 child_classif_btn3.pack(fill=X)
+
+child_classif_btn5 = Button(child_classif, text="Naive Bayes Classifier")
+child_classif_btn5.pack(fill=X)
+
+child_classif_btn6 = Button(child_classif, text="Алгоритм опорных векторов")
+child_classif_btn6.pack(fill=X)
+
+child_classif_btn7 = Button(child_classif, text="Алгоритм Роккио")
+child_classif_btn7.pack(fill=X)
 
 child_classif_btn4 = Button(child_classif, text="Назад")
 child_classif_btn4.bind("<Button-1>", show_hide_child_classif)
@@ -107,11 +116,17 @@ child_claster_btn1 = Button(child_claster, text="Mean Shift")
 child_claster_btn1.bind("<Button-1>", mean_shift_run)
 child_claster_btn1.pack(fill=X)
 
-child_claster_btn2 = Button(child_claster, text="Метод класстеризация 2")
+child_claster_btn2 = Button(child_claster, text="K-means")
 child_claster_btn2.pack(fill=X)
 
-child_claster_btn3 = Button(child_claster, text="Метод класстеризация 3")
+child_claster_btn3 = Button(child_claster, text="Hierarchical clustering")
 child_claster_btn3.pack(fill=X)
+
+child_claster_btn5 = Button(child_claster, text="DBSCAN")
+child_claster_btn5.pack(fill=X)
+
+child_claster_btn6 = Button(child_claster, text="Affinity Propagation")
+child_claster_btn6.pack(fill=X)
 
 child_claster_btn4 = Button(child_claster, text="Назад")
 child_claster_btn4.bind("<Button-1>", show_hide_child_claster)
@@ -125,14 +140,11 @@ child_search_rules.geometry("400x200")
 child_search_rules.bind("<Destroy>", exit_prog);
 child_search_rules.withdraw()
 
-child_search_rules_btn1 = Button(child_search_rules, text="Метод поиск ассоциативных правил 3")
+child_search_rules_btn1 = Button(child_search_rules, text="Apriori Hybrid")
 child_search_rules_btn1.pack(fill=X)
 
-child_search_rules_btn2 = Button(child_search_rules, text="Метод поиск ассоциативных правил 3")
+child_search_rules_btn2 = Button(child_search_rules, text="ID3")
 child_search_rules_btn2.pack(fill=X)
-
-child_search_rules_btn3 = Button(child_search_rules, text="Метод поиск ассоциативных правил 3")
-child_search_rules_btn3.pack(fill=X)
 
 child_search_rules_btn4 = Button(child_search_rules, text="Назад")
 child_search_rules_btn4.bind("<Button-1>", show_hide_child_search_rules)
