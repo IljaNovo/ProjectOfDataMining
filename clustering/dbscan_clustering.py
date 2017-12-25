1eef0e887b79c49b5699bdb289feb51c7b43775b
Vladimir Kornilov <vladimir.kornilov@accenture.com>
2017-12-25 15:02:16 +0300

    Add text output of metrics DBSCAN

--- clustering/dbscan_clustering.py
+++ clustering/dbscan_clustering.py
@@ -31,13 +31,14 @@ def dbscan_run(X, eps_, min_samples_):
 
     # Number of clusters in labels, ignoring noise if present.
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-    print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-    print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-    print("Adjusted Rand Index: %0.3f"
-          % metrics.adjusted_rand_score(labels_true, labels))
-    print("Adjusted Mutual Information: %0.3f"
+    io.Output.write_to_txt_file("!Results/clustering_DBSCAN_metrics_result.txt",
+    'Estimated number of clusters: %d' % n_clusters_ + "\n" +
+    "Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels)+ "\n" +
+    "Completeness: %0.3f" % metrics.completeness_score(labels_true, labels)+ "\n" +
+    "V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels)+ "\n" +
+    "Adjusted Rand Index: %0.3f"
+          % metrics.adjusted_rand_score(labels_true, labels)+ "\n" +
+    "Adjusted Mutual Information: %0.3f"
           % metrics.adjusted_mutual_info_score(labels_true, labels))
 
     ##############################################################################
